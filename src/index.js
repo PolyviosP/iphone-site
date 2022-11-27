2e542fad35ea186ec81527b0b2f108fce5ee6f93
PolyviosP <patseadisp@gmail.com>
2022-11-27 18:55:58 +0200

    First commit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,22 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+/*const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json())
+
+app.post('/api/', (req, res) => {
+    res.send('YOOO');
+});
+
+app.get('/api/courses', (req, res) => {
+    res.send([1,2,3]);
+});
+
+//app.listen(PORT, () => console.log('akouo sthn porta 3000'))*/
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
